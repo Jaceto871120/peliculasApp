33bc8b3ce191491aa33a24484baba20a6c7d3a51
Jair Armando Celis Torrado <tecnologico@ticketshop.com.co>
2019-05-30 18:37:37 -0500

    Se crea el proyecto

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  url: 'https://api.themoviedb.org/3',
+  apiKey: 'da522db32b7ca2643f76d21eaffc62cb',
+  imgPath: 'https://image.tmdb.org/t/p',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  url: 'https://api.themoviedb.org/3',
+  apiKey: 'da522db32b7ca2643f76d21eaffc62cb',
+  imgPath: 'https://image.tmdb.org/t/p',
 };
 
 /*
