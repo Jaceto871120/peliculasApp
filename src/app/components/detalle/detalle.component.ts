a16fbf7d1c2d043e6f760c00a6ca2eec1449b714
Jair Armando Celis Torrado <tecnologico@ticketshop.com.co>
2019-05-31 16:49:26 -0500

    Se genera el html para ver los detalles de cada pelicula

--- src/app/components/detalle/detalle.component.ts
+++ src/app/components/detalle/detalle.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { MoviesService } from '../../services/movies.service';
+import { PeliculaDetalle, Cast } from '../../interfaces/interfaces';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-detalle',
@@ -10,18 +12,44 @@ export class DetalleComponent implements OnInit {
 
   @Input() id;
 
-  constructor(private moviesService: MoviesService) { }
+  slideOpts = {
+    slidesPerView: 3.2,
+    freeMode: true,
+    spaceBetween: -10,
+  };
+
+  actores: Cast[] = [];
+
+  ocultar = 150;
+
+  pelicula: PeliculaDetalle = {};
+
+  constructor(private moviesService: MoviesService,
+              private modalController: ModalController) { }
 
   ngOnInit() {
     console.log('ID', this.id);
     this.moviesService.getPeliculaDetalle(this.id)
                       .subscribe( resp => {
                         console.log('Detalles', resp);
+                        this.pelicula = resp;
                       });
 
     this.moviesService.getActoresPelicula(this.id)
                       .subscribe( resp => {
                         console.log('Actores', resp);
+                        this.actores = resp.cast;
                       });
+  }
+  onClick() {
+    this.ocultar = 5000;
+  }
+
+  regresar() {
+    this.modalController.dismiss();
+  }
+
+  favorito() {
+
   }
 }
